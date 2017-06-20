d4007932280fe6aec0244391df4421580968acf4
Bianca <bianca.alves2011@hotmail.com>
2017-06-20 18:15:29 -0300

    Classe Sql adicionada

--- index.php
+++ index.php
@@ -1,13 +1,25 @@
 <?php 
+//Require do composer
+require_once("vendor/autoload.php");
 
-/* 
+//Instanciando a classe Slim
+$app = new \Slim\Slim();
 
-Curso PHP7 - Projeto final
+//Slim: Configurando para que sejam mostrados todas as mensagens de erro
+$app->config('debug', true);
 
-Esse projeto contém um mecanismo de login e senha e um CRUD - necessário em qualquer projeto.
+//Slim: Criando conteúdo para o caminho "/"
+$app->get('/', function() {
 
-*/
+	$sql = new Hcode\DB\Sql();
 
+	$results = $sql->select("SELECT * FROM tb_users");
 
+	echo json_encode($results);
 
-?>
+});
+
+//Rodando o Slim
+$app->run();
+
+ ?>
