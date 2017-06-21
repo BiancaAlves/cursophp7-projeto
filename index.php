27fc1ac552e6970838ef50f0d12cdf5cc6a3cb5a
Bianca <bianca.alves2011@hotmail.com>
2017-06-21 19:39:15 -0300

    Tela de login configurada

--- index.php
+++ index.php
@@ -1,10 +1,13 @@
 <?php 
+session_start();
+
 //Require do composer
 require_once("vendor/autoload.php");
 
 use \Slim\Slim;
 use \Hcode\Page;
 use \Hcode\PageAdmin;
+use \Hcode\Model\User;
 
 //Instanciando a classe Slim
 $app = new Slim();
@@ -22,12 +25,37 @@ $app->get('/', function() {
 });
 
 //Slim: Criando conteúdo para o caminho "/admin"
-$app->get('/admin', function() {
+$app->get('/admin/', function() {
+	User::verifyLogin();
 
 	$page = new PageAdmin();
 
 	$page->setTpl("index");
 
+});
+
+
+$app->get('/admin/login', function(){
+	$page = new PageAdmin(array(
+		"header"=>false,
+		"footer"=>false
+		));
+	$page->setTpl("login");
+});
+
+$app->post('/admin/login', function(){
+
+	User::login($_POST["login"], $_POST["password"]);
+	header("Location: /admin");
+	exit;
+});
+
+$app->get('/admin/logout', function(){
+
+	User::logout();
+
+	header("Location: /admin/login");
+	exit;
 });
 
 //Rodando o Slim
